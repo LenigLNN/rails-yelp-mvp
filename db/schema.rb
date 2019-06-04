92d88e44a00912a5f64b5eaf9667ddbf5cdd19d1
Lena Nigrell <nigrell@live.se>
2019-06-04 12:22:23 +0200

    controllers done

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 2019_06_04_090349) do
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "address"
-    t.string "phone"
+    t.string "phone_number"
     t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number: '0703646166',
+    category:     'japanese'
+  },
+  {
+    name:         'Burger King',
+    address:      'Skolgatan',
+    phone_number: '0707776166',
+    category:     'french'
+  },
+  {
+    name:         'Italian Nice',
+    address:      'Gurkgränd',
+    phone_number: '0700077166',
+    category:     'italian'
+  },
+  {
+    name:         'Smått o gott',
+    address:      'Near by',
+    phone_number: '0707700166',
+    category:     'chinese'
+  },
+  {
+    name:         'Annat',
+    address:      'where',
+    phone_number: '0707700898',
+    category:     'belgian'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
