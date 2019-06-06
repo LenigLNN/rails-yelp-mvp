ce090020a1d1d7d00c066c162037c4feb9d94dd0
Lena Nigrell <nigrell@live.se>
2019-06-06 12:48:04 +0200

    change of presence-true to allow_blank-false for better error message on category_

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -4,6 +4,7 @@ class Restaurant < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :address, presence: true
   validates :category, inclusion: {
-    in: ['chinese', 'italian', 'japanese', 'french', 'belgian']
-  }, presence: true
+    in: ['chinese', 'italian', 'japanese', 'french', 'belgian'],
+    allow_nil: false
+  }
 end
